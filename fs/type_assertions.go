1f0f599f255dab02f7c5adb71df923788ace8d46
James Swineson <github@public.swineson.me>
2022-03-18 19:39:32 +0800

    fs: add Exists, IsDir, IsFile

---
+++ fs/type_assertions.go
@@ -0,0 +1,27 @@
+package fs
+
+import "os"
+
+// Exists determines if the path points to something at this time.
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+// IsDir determines if the path represents a directory (folder).
+func IsDir(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return info.IsDir()
+}
+
+// IsFile determines if the path represents a file. Anything on the filesystem other than a directory is a file.
+func IsFile(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
