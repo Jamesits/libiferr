1e9fb117bfc7f3b38cff8f752665d410cb6f3282
James Swineson <github@public.swineson.me>
2023-03-13 01:14:51 +0800

    testing_utils: add AssertSameSize

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/jamesits/libiferr
 
-go 1.16
+go 1.18
 
 require (
 	github.com/sirupsen/logrus v1.8.1
---
+++ testing_utils/padding_check.go
@@ -0,0 +1,14 @@
+package testing_utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"unsafe"
+)
+
+// AssertSameSize tests if 2 data types are of the same size in memory.
+// It is designed to check for things like C unions, where one struct need to be manually padded to the size of another
+// struct.
+func AssertSameSize[Expected any, Actual any](t *testing.T) {
+	assert.EqualValues(t, unsafe.Sizeof(*new(Expected)), unsafe.Sizeof(*new(Actual)))
+}
